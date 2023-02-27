3363d25722a0d83b6098f03dbb766765ede1a2cb
Radison Akerman <rak3rman@gmail.com>
2023-02-26 20:31:46 -0600

    feat: zerosToTheFront implemented

--- src/zerosFront.cpp
+++ src/zerosFront.cpp
@@ -15,5 +15,24 @@
  * @param {int} count - number of elements in array
  */
 void zerosToTheFront(int *array, int count) {
-
+  // 1. Edge case, empty array or count out of bounds
+  if (array == nullptr || count < 1) return;
+  // 2. Loop through the array, starting at index 0
+  int cursor = 0;
+  int swap = 0;
+  for (int i = 0; i < count; i++) {
+    // Check for 0, if not 0, advance cursor
+    if (array[i] != 0) {
+      // Non-zero, advance cursor
+      cursor++;
+    } else {
+      // Zero, swap at swap pos
+      int temp = array[swap];
+      array[swap] = array[i];
+      array[i] = temp;
+      swap++;
+    }
+  }
+  // 3. Return, with zeros in place in array
+  return;
 }
--- tests.cpp
+++ tests.cpp
@@ -16,6 +16,144 @@
 
 using namespace std;
 
+/* **************************************************
+ * zerosToTheFront()
+ * *************************************************/
+
+// NOTE: Test cases not written for the edge case where
+//       count is beyond amount allocated for array,
+//       results in undefined behavior
+
+TEST(zerosToTheFrontTest, emptyArrZeroCount) {
+    int *array = nullptr;
+    int count = 0;
+
+    zerosToTheFront(array, count);
+    ASSERT_EQ(array, nullptr);
+}
+
+TEST(zerosToTheFrontTest, emptyArrPosCount) {
+  int *array = nullptr;
+  int count = 2;
+
+  zerosToTheFront(array, count);
+  ASSERT_EQ(array, nullptr);
+}
+
+TEST(zerosToTheFrontTest, emptyArrNegCount) {
+  int *array = nullptr;
+  int count = -1;
+
+  zerosToTheFront(array, count);
+  ASSERT_EQ(array, nullptr);
+}
+
+TEST(zerosToTheFrontTest, seqArrValidCount) {
+  int *array = new int[10];
+  for (int i = 0; i < 10; i++) { array[i] = i; }
+  int count = 10;
+
+  zerosToTheFront(array, count);
+  for (int i = 0; i < 10; i++) { ASSERT_EQ(array[i], i); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, seqArrValidCountBig) {
+  int *array = new int[10000];
+  for (int i = 0; i < 10000; i++) { array[i] = i; }
+  int count = 10000;
+
+  zerosToTheFront(array, count);
+  for (int i = 0; i < 10000; i++) { ASSERT_EQ(array[i], i); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, seqArrUnderCount) {
+  int *array = new int[10];
+  for (int i = 0; i < 10; i++) { array[i] = i; }
+  int count = 8;
+
+  zerosToTheFront(array, count);
+  for (int i = 0; i < 10; i++) { ASSERT_EQ(array[i], i); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, seqRevArrValidCount) {
+  int *array = new int[10];
+  for (int i = 0; i < 10; i++) { array[i] = 9 - i; }
+  int count = 10;
+
+  zerosToTheFront(array, count);
+  for (int i = 0; i < 1; i++) { ASSERT_EQ(array[i], 0); }
+  for (int i = 1; i < 10; i++) { ASSERT_NE(array[i], 0); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, seqRevArrUnderCount) {
+  int *array = new int[10];
+  for (int i = 0; i < 10; i++) { array[i] = 9 - i; }
+  int count = 9;
+
+  zerosToTheFront(array, count);
+  for (int i = 9; i < 10; i++) { ASSERT_EQ(array[i], 0); }
+  for (int i = 0; i < 9; i++) { ASSERT_NE(array[i], 0); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, balArrValidCount) {
+  int *array = new int[20];
+  for (int i = 0; i < 20; i++) { array[i] = 9 - i; }
+  int count = 20;
+
+  zerosToTheFront(array, count);
+  for (int i = 0; i < 1; i++) { ASSERT_EQ(array[i], 0); }
+  for (int i = 1; i < 20; i++) { ASSERT_NE(array[i], 0); }
+
+  delete[] array;
+}
+
+TEST(zerosToTheFrontTest, randArrValidCount) {
+  for (int count = 0; count < 50; count++) {
+    int *array = new int[count];
+    for (int i = 0; i < count; i++) { array[i] = rand() % 100 - 50; } // Picks random -50 to 50
+
+    int zeros = 0;
+    for (int i = 0; i < count; i++) {
+      if (array[i] == 0) zeros++;
+    }
+
+    zerosToTheFront(array, count);
+    for (int i = 0; i < zeros; i++) { ASSERT_EQ(array[i], 0); }
+    for (int i = zeros; i < count; i++) { ASSERT_NE(array[i], 0); }
+
+    delete[] array;
+  }
+}
+
+TEST(zerosToTheFrontTest, randArrRandCount) {
+  for (int count = 0; count < 500; count++) {
+    int *array = new int[count];
+    for (int i = 0; i < count; i++) { array[i] = rand() % 100 - 50; } // Picks random -50 to 50
+    int randCount = count - rand() % 5; // Redefine count
+
+    int zeros = 0;
+    for (int i = 0; i < randCount; i++) {
+      if (array[i] == 0) zeros++;
+    }
+
+    zerosToTheFront(array, randCount);
+    for (int i = 0; i < zeros; i++) { ASSERT_EQ(array[i], 0); }
+    for (int i = zeros; i < randCount; i++) { ASSERT_NE(array[i], 0); }
+
+    delete[] array;
+  }
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
